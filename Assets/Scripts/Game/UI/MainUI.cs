c0d6acf2088aeb3704ad1f76a3e9162efcf3d3ff
ALEXTANG <574809918@qq.com>
2021-12-21 20:53:05 +0800

    Update
    Update

--- Assets/Scripts/Game/UI/MainUI.cs
+++ Assets/Scripts/Game/UI/MainUI.cs
@@ -5,14 +5,11 @@ namespace Assets.Scripts.Game.UI
 {
     public class MainUI : MonoBehaviour
     {
-
-        // Use this for initialization
         void Start()
         {
 
         }
 
-        // Update is called once per frame
         void Update()
         {
 
